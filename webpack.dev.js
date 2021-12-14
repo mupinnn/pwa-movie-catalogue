ed62e3544d9346870cd59dec5d63ae7d9549aa63
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-14 13:33:27 +0700

    chore(webpack): minimal setup

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const commonConfig = require('./webpack.common');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+});

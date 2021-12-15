aee62819a59902abae631c5882ec831cf1c58b43
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-15 13:39:10 +0700

    feat: tmdb api config

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  API_KEY: "37f7c26ea98617a446ac664e3f1348b5",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMG_URL: "https://api.themoviedb.org/3/",
+  DEFAUL_LANG: "en-us"
+};
+
+export default CONFIG;

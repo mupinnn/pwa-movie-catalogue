f07eefda3e2c894a07330846601525ce99bdafe4
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-15 14:13:16 +0700

    feat(now-playing): render data

--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -1,12 +1,22 @@
+import TMDBSource from '../../data/tmdb-source';
+import { createMovieItemTemplate } from '../templates/template-creator';
+
 const NowPlaying = {
   async render() {
     return `
-      <h2>Now Playing Page</h2>
+      <div class"content">
+        <h2 class="content__heading">Now Playing in Cinema</h2>
+        <div id="movies" class="movies"></div>
+      </div>
     `;
   },
 
   async afterRender() {
-    //
+    const movies = await TMDBSource.fetchNowPlayingMovies();
+    const movieContainer = document.getElementById('movies');
+    movies.forEach((movie) => {
+      movieContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 

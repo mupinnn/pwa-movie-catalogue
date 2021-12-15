53acc67805dcdb6cadb0f14e855e3f22d56f4d46
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-15 14:20:38 +0700

    feat(detail): render data

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,19 @@
+import TMDBSource from '../../data/tmdb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
+import URLParser from '../../routes/url-parser';
+
 const Detail = {
   async render() {
     return `
-      <h2>Detail Page</h2>
+      <div id="movie" class="movie"></div>
     `;
   },
 
   async afterRender() {
-    //
+    const url = URLParser.parseActiveURLWithoutCombiner();
+    const movie = await TMDBSource.fetchMovieDetail(url.id);
+    const movieContainer = document.getElementById('movie');
+    movieContainer.innerHTML = createMovieDetailTemplate(movie);
   },
 };
 

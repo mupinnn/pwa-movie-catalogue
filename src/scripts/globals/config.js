f01fdfc985a92d345bd26f5cf4284666b552b239
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-15 14:11:37 +0700

    feat: fix API constant typo and wrong image url

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-  API_KEY: "37f7c26ea98617a446ac664e3f1348b5",
-  BASE_URL: "https://api.themoviedb.org/3/",
-  BASE_IMG_URL: "https://api.themoviedb.org/3/",
-  DEFAUL_LANG: "en-us"
+  API_KEY: '37f7c26ea98617a446ac664e3f1348b5',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMG_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANG: 'en-us',
 };
 
 export default CONFIG;

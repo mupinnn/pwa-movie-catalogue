6894c51e08df48af2696f86bc702eba3b25a32b2
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-18 14:34:01 +0700

    feat: idb sourcing helper

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,6 +4,9 @@ const CONFIG = {
   BASE_IMG_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANG: 'en-us',
   CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
 };
 
 export default CONFIG;

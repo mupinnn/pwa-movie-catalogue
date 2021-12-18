b97d6d82dcd76989521974a17b2d0cda72d1c5d8
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-18 15:35:43 +0700

    feat: create liked movie page

---
+++ src/scripts/views/pages/like.js
@@ -0,0 +1,23 @@
+import FavoritMovieIdb from '../../data/favoritemovie-idb';
+import { createMovieItemTemplate } from '../templates/template-creator';
+
+const Like = {
+  async render() {
+    return `
+      <div class="content">
+        <h2 class="content__heading">Your Liked Movie</h2>
+        <div id="movies" class="movies"></div>
+      </div>
+    `;
+  },
+
+  async afterRender() {
+    const movies = await FavoritMovieIdb.getAllMovie();
+    const moviesContainer = document.getElementById('movies');
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
+  },
+};
+
+export default Like;

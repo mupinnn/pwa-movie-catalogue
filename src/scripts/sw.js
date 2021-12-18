b7463fccc8b757667de680f457ba9c343ee0cffe
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-18 12:35:38 +0700

    feat: service worker caching

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-restricted-globals */
+
+// daftar aset yang secara otomatis dibuat oleh serviceworker-webpack-plugin
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+const { assets } = global.serviceWorkerOption;
+
 self.addEventListener('install', (event) => {
-  console.log('Installing service worker...');
+  self.skipWaiting();
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating service worker...');
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Request: ', event.request);
-
-  event.waitUntil(fetch(event.request));
+  event.waitUntil(CacheHelper.revalidateCache(event.request));
 });
